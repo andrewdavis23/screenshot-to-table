288a12bc9161c298b3004b5ec2223c94dd60d1be
andrewdavis23 <47924318+andrewdavis23@users.noreply.github.com>
2021-07-04 15:53:52 -0400

    Add files via upload
    Adjusted for erosion/dilation bug. Worked up to creating grid lines.

--- screenshot to table demo.py
+++ screenshot to table demo.py
@@ -1,3 +1,8 @@
+# Screenshot to table
+# Andrew Davis personal project
+# July 2021
+### Bug: dilate and erode functionality are switched in cv2
+
 from tkinter import *
 from tkinter import filedialog
 import numpy as np
@@ -32,24 +37,21 @@ except ImportError:
 # standard, black all borders format
 file = r'//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/all borders simple.jpg'
 
+# PARAMETERS
+# proportional size of kernel kernel_length= 1/kern_p
+kern_p = 50
+# used to erode and dilate pixels to get lines
+vert_iter = 8
+horz_iter = 8
+
 # read file, flag=0 tells imread to return grayscale image
 img = cv2.imread(file, 0)
 
-# Plot original image
-plt.imshow(img,cmap='gray')
-plt.title('Screenshot')
-plt.show()
-
 # thresholding the image to a binary image
-thresh,img_bin = cv2.threshold(img,0,255,cv2.THRESH_BINARY |cv2.THRESH_OTSU)
-
-# Plot the binary image
-plt.imshow(img_bin,cmap='gray')
-plt.title('Binary Image. Otsu Optimized threshold = '+str(thresh))
-plt.show()
+thresh, img_bin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY |cv2.THRESH_OTSU)
 
 # Length(width) of kernel as 100th of total width
-kernel_len = np.array(img).shape[1]//100
+kernel_len = np.array(img).shape[1]//kern_p
 
 # Defining a vertical kernel to detect all vertical lines of image 
 ver_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_len))
@@ -60,54 +62,81 @@ hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_len, 1))
 # A kernel of 2x2
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 
-##################
+# Kernal Stats
+############## TEST ###################################################################
 print("\nimage width\n",np.array(img).shape[1],"\nkernel length\n",kernel_len,"\nvertical kernel\n",ver_kernel,"\nhorizontal kernel\n",hor_kernel,"\nkernel\n",kernel)
+########################################################################################
 
-# Use vertical kernel to detect and save the vertical lines in a jpg
-# erosion, dilation used in sequence to remove noise
-image_1 = cv2.erode(img_bin, ver_kernel, iterations=3)
-vertical_lines = cv2.dilate(image_1, ver_kernel, iterations=3)
+### Bug: dilate and erode functionality are switched in cv2
+image_1 = cv2.dilate(img_bin, ver_kernel, iterations=vert_iter)
+vertical_lines = cv2.erode(image_1, ver_kernel, iterations=vert_iter)
 cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/vertical.jpg", vertical_lines)
 
-plt.imshow(image_1,cmap='gray')
-plt.title('cv2.erode(img_bin, ver_kernel, iterations=3)')
+figure, axis = plt.subplots(2, 2)
+# Plot original image
+axis[0, 0].imshow(img,cmap='gray')
+axis[0, 0].set_title('Screenshot')
+
+# Plot the binary image
+axis[0, 1].imshow(img_bin,cmap='gray')
+axis[0, 1].set_title('Binary Image. Otsu Optimized threshold = '+str(thresh))
+
+# Vertical erosion
+axis[1, 0].imshow(image_1,cmap='gray')
+axis[1, 0].set_title('cv2.erode(img_bin, ver_kernel, iterations={}) len(ver_kernel)={}'.format(vert_iter,len(ver_kernel)))
+
+# Vertical dilation
+axis[1, 1].imshow(vertical_lines,cmap='gray')
+axis[1, 1].set_title('cv2.dilate(image_1, ver_kernel, iterations={})'.format(vert_iter))
 plt.show()
 
-plt.imshow(vertical_lines,cmap='gray')
-plt.title('cv2.dilate(image_1, ver_kernel, iterations=3)')
-plt.show()
-
-###################
-exit()
-
+### Bug: dilate and erode functionality are switched in cv2
 # Use horizontal kernel to detect and save the horizontal lines in a jpg
-image_2 = cv2.erode(img_bin, hor_kernel, iterations=3)
-horizontal_lines = cv2.dilate(image_2, hor_kernel, iterations=3)
+image_2 = cv2.dilate(img_bin, hor_kernel, iterations=horz_iter)
+horizontal_lines = cv2.erode(image_2, hor_kernel, iterations=horz_iter)
 cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/horizontal.jpg",horizontal_lines)
-
-plt.imshow(horizontal_lines,cmap='gray')
-plt.title('horizontal lines')
-plt.show()
 
 # Combine horizontal and vertical lines in a new third image, with both having same weight.
 grid = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)
 
+# GRID CREATION DEMO
+figure, axis = plt.subplots(2, 2)
+# Plot original image
+axis[0, 0].imshow(vertical_lines,cmap='gray')
+axis[0, 0].set_title('vertical lines')
+
+axis[0, 1].imshow(horizontal_lines,cmap='gray')
+axis[0, 1].set_title('horizontal lines')
+
+axis[1, 0].imshow(grid,cmap='gray')
+axis[1, 0].set_title('cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)')
+
 # Eroding and thesholding the image
 grid = cv2.erode(~grid, kernel, iterations=2)
 
-# Plot grid
-plotting = plt.imshow(grid, cmap='gray')
-plt.title('Combination of Vertical and Horizontal Pixels')
+axis[1, 1].imshow(vertical_lines,cmap='gray')
+axis[1, 1].set_title('cv2.erode(~grid, kernel, iterations=2)')
 plt.show()
 
-thresh, grid = cv2.threshold(grid,128,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/grid.jpg", grid)
+# thresh, grid = cv2.threshold(grid, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+# cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/grid.jpg", grid)
 
 bitxor = cv2.bitwise_xor(img_bin, grid)
 bitnot = cv2.bitwise_not(bitxor)
 
-# Plot binary image with grid exlcuded
-plotting = plt.imshow(bitnot, cmap='gray')
-plt.title('Binary Image with Grid Exlcuded')
+# GRID LOGIC DEMO
+figure, axis = plt.subplots(2, 2)
+axis[0, 0].imshow(img_bin,cmap='gray')
+axis[0, 0].set_title('Binary Image')
+
+axis[0, 1].imshow(grid,cmap='gray')
+axis[0, 1].set_title('Grid')
+
+axis[1, 0].imshow(bitxor,cmap='gray')
+axis[1, 0].set_title('cv2.bitwise_xor(img_bin, grid)')
+
+axis[1, 1].imshow(bitnot,cmap='gray')
+axis[1, 1].set_title('cv2.bitwise_not(bitxor)')
+
 plt.show()
 
