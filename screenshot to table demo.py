1e44118899309858834cd7ee7b02f35d6fb07c5d
andrewdavis23 <47924318+andrewdavis23@users.noreply.github.com>
2021-08-13 10:59:01 -0400

    Add files via upload
    boxes overlaying the original image

--- screenshot to table demo.py
+++ screenshot to table demo.py
@@ -52,6 +52,7 @@ thresh, img_bin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY |cv2.THRESH_OTSU)
 
 # Length(width) of kernel as 100th of total width
 kernel_len = np.array(img).shape[1]//kern_p
+if kernel_len%2==0: kernel_len+=1
 
 # Defining a vertical kernel to detect all vertical lines of image 
 ver_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_len))
@@ -62,33 +63,10 @@ hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_len, 1))
 # A kernel of 2x2
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 
-# Kernal Stats
-############## TEST ###################################################################
-print("\nimage width\n",np.array(img).shape[1],"\nkernel length\n",kernel_len,"\nvertical kernel\n",ver_kernel,"\nhorizontal kernel\n",hor_kernel,"\nkernel\n",kernel)
-########################################################################################
-
-### Bug: dilate and erode functionality are switched in cv2
+# get vertical lines
 image_1 = cv2.dilate(img_bin, ver_kernel, iterations=vert_iter)
 vertical_lines = cv2.erode(image_1, ver_kernel, iterations=vert_iter)
 cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/vertical.jpg", vertical_lines)
-
-figure, axis = plt.subplots(2, 2)
-# Plot original image
-axis[0, 0].imshow(img,cmap='gray')
-axis[0, 0].set_title('Screenshot')
-
-# Plot the binary image
-axis[0, 1].imshow(img_bin,cmap='gray')
-axis[0, 1].set_title('Binary Image. Otsu Optimized threshold = '+str(thresh))
-
-# Vertical erosion
-axis[1, 0].imshow(image_1,cmap='gray')
-axis[1, 0].set_title('cv2.erode(img_bin, ver_kernel, iterations={}) len(ver_kernel)={}'.format(vert_iter,len(ver_kernel)))
-
-# Vertical dilation
-axis[1, 1].imshow(vertical_lines,cmap='gray')
-axis[1, 1].set_title('cv2.dilate(image_1, ver_kernel, iterations={})'.format(vert_iter))
-plt.show()
 
 ### Bug: dilate and erode functionality are switched in cv2
 # Use horizontal kernel to detect and save the horizontal lines in a jpg
@@ -97,46 +75,64 @@ horizontal_lines = cv2.erode(image_2, hor_kernel, iterations=horz_iter)
 cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/horizontal.jpg",horizontal_lines)
 
 # Combine horizontal and vertical lines in a new third image, with both having same weight.
-grid = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)
+grid = cv2.addWeighted(vertical_lines, 0.2, horizontal_lines, 0.2, 0.0)
 
-# GRID CREATION DEMO
-figure, axis = plt.subplots(2, 2)
-# Plot original image
-axis[0, 0].imshow(vertical_lines,cmap='gray')
-axis[0, 0].set_title('vertical lines')
-
-axis[0, 1].imshow(horizontal_lines,cmap='gray')
-axis[0, 1].set_title('horizontal lines')
-
-axis[1, 0].imshow(grid,cmap='gray')
-axis[1, 0].set_title('cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)')
-
-# Eroding and thesholding the image
-grid = cv2.erode(~grid, kernel, iterations=2)
-
-axis[1, 1].imshow(vertical_lines,cmap='gray')
-axis[1, 1].set_title('cv2.erode(~grid, kernel, iterations=2)')
-plt.show()
-
-# thresh, grid = cv2.threshold(grid, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-# cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/grid.jpg", grid)
+thresh, grid = cv2.threshold(grid, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+cv2.imwrite("//n0adcdata3/redirect$/nuajd15/My Documents/Python/image to table/grid.jpg", grid)
 
 bitxor = cv2.bitwise_xor(img_bin, grid)
 bitnot = cv2.bitwise_not(bitxor)
 
-# GRID LOGIC DEMO
-figure, axis = plt.subplots(2, 2)
-axis[0, 0].imshow(img_bin,cmap='gray')
-axis[0, 0].set_title('Binary Image')
+# DEMO
+figure, axis = plt.subplots(2,3)
+axis[0,0].imshow(img_bin, cmap='gray')
+axis[0,0].set_title('binary image')
+axis[0,1].imshow(grid, cmap='gray')
+axis[0,1].set_title('vert/horz combo')
+axis[0,2].imshow(bitnot, cmap='gray')
+axis[0,2].set_title('pixels not in grid')
 
-axis[0, 1].imshow(grid,cmap='gray')
-axis[0, 1].set_title('Grid')
+# Detect contours for following box detection
+contours, hierarchy = cv2.findContours(grid, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-axis[1, 0].imshow(bitxor,cmap='gray')
-axis[1, 0].set_title('cv2.bitwise_xor(img_bin, grid)')
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b:b[1][i], reverse=reverse))
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
 
-axis[1, 1].imshow(bitnot,cmap='gray')
-axis[1, 1].set_title('cv2.bitwise_not(bitxor)')
+# Sort all the contours by top to bottom.
+contours, boundingBoxes = sort_contours(contours, method="top-to-bottom")
+
+#Creating a list of heights for all detected boxes
+heights = [boundingBoxes[i][3] for i in range(len(boundingBoxes))]
+#Get mean of heights
+mean = np.mean(heights)
+
+# Get position (x,y), width and height for every contour
+box = []
+for c in contours:
+    x, y, w, h = cv2.boundingRect(c)
+    if (w<1000 and h<500):
+        image = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        box.append([x,y,w,h])
+
+axis[1,0].imshow(image,cmap='gray')
+axis[1,0].set_title('boxes over image')
+
+
 
 plt.show()
 
